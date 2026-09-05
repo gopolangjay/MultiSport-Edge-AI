c766c0337c51272894519aff1a3a50bef28c2f04
gopolangjay <gopolangjay@gmail.com>
2026-09-05 14:08:35 +0200

    Configure MultiSport Android app

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "za.co.multisportedge"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "za.co.multisportedge"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
